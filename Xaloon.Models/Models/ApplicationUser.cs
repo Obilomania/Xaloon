c5165d69bb6749e13469be122f063d0092059805
Obilomania <iloanyaobinna@gmail.com>
2022-08-19 01:16:12 +0100

    Added Identity

---
+++ Xaloon.Models/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xaloon.Models.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
